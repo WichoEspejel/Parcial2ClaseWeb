53d753d94f5fcc11221ac9f7578aafc6a2a9e677
Wicho <wichyn@gmail.com>
2015-10-30 16:51:14 -0600

    Codigo inicial

---
+++ twitter/config.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+require 'vendor/autoload.php';
+
+define('CONSUMER_KEY', 'HiSjYvs448mdbqWw9ak5ozrdN');
+define('CONSUMER_SECRET', 'fP6czdlIXfuXhhi8XeAJCa2Whg2XZUFTHkBsVzYBPIWOW1eRIy');
+define('OAUTH_CALLBACK', 'http://ec2-54-218-84-4.us-west-2.compute.amazonaws.com/twitter/callback.php');
+
+?>
